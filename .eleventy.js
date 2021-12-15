9862b3761aa94909aed6f8c3e3ff143e7b92248a
“[firstname <ashirvadsatapathy2828@gmail.com>
2021-12-15 22:31:09 +0530

    init

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addWatchTarget("./src/sass/");
+    eleventyConfig.addPassthroughCopy("./src/css/");
+    eleventyConfig.addPassthroughCopy("./src/assets");
+    eleventyConfig.addPassthroughCopy("./src/js");
+
+    return {
+        dir: {
+            input : "src",
+            output : "public",
+        },
+    };
+}
---
+++ src/app.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCO0Tr2ZVMym6SeouQu9oRiO1zPDq5F3xk",
+    authDomain: "data28-35410.firebaseapp.com",
+    projectId: "data28-35410",
+    storageBucket: "data28-35410.appspot.com",
+    messagingSenderId: "411051427941",
+    appId: "1:411051427941:web:bc75b6acb177b95a6b1e8c"
+};
+
+const app = initializeApp(firebaseConfig);
